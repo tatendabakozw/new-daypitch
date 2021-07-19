812a0868feccde62df5c388417f0a265fc3c191b
tatendabakozw <tatendabakozw@gmail.com>
2021-07-19 21:49:56 +0200

    mobile menu icon

--- src/components/navigation/HomeNavbar.jsx
+++ src/components/navigation/HomeNavbar.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+import {MenuAlt1Icon, ChevronDownIcon} from '@heroicons/react/outline'
 
-function HomeNavbar() {
+
+function HomeNavbar(props) {
+    const [navbaron, setNavbarOn] = useState(false)
+
+      const changeBackground = () => {
+        if (window.scrollY >= 100) {
+            setNavbarOn(true)
+        } else {
+            setNavbarOn(false)
+        }
+    }
+
+    window.addEventListener('scroll', changeBackground)
+
     return (
-        <div>
-            home navbar
-        </div>
+      <>
+        <nav className={`${navbaron ? 'bg-gray-50' : 'bg-white'} bg-white lg:px-24 md:px-16 px-4 h-16 flex flex-row items-center w-full transition duration-500 ease-in-out`}>
+          <div className="flex flex-row items-center justify-between w-full">
+            <div className="">
+              <p>logo</p>
+            </div>
+            <div className="lg:flex md:flex hidden">
+              <p>other</p>
+            </div>
+            <div className="lg:hidden md:hidden flex">
+              <MenuAlt1Icon height={24} width={24} className="text-gray-700" />
+            </div>
+          </div>
+        </nav>
+      </>
     )
 }
 
--- src/layouts/HomeLayout/HomeLayout.jsx
+++ src/layouts/HomeLayout/HomeLayout.jsx
@@ -4,12 +4,13 @@ import HomeNavbar from '../../components/navigation/HomeNavbar'
 function HomeLayout({children}) {
     return (
         <div className="layout">
-            <div className="navbar">
+            <div className="fixed w-full top-0 z-50">
                 <HomeNavbar/>
             </div>
-            <div className="body">
+            <div className="min-h-screen bg-red-500 pt-16">
                 {children}
             </div>
+            <div className="h-screen bg-green-600"></div>
         </div>
     )
 }

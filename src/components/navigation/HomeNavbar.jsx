7a8dd91773036692ed9b1a92ac6b85b055d3af54
tatendabakozw <tatendabakozw@gmail.com>
2021-07-19 22:08:36 +0200

    mobile dropdown

--- src/components/navigation/HomeNavbar.jsx
+++ src/components/navigation/HomeNavbar.jsx
@@ -1,17 +1,35 @@
 import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import {MenuAlt1Icon, ChevronDownIcon} from '@heroicons/react/outline'
+import NavbarDropdown from '../dropdowns/NavbarDropdown'
 
+const UnAuthenticatedNaOptions = [
+  {name : 'Explore', location:'/explore', id:"ias878"},
+  {name : 'How It Works', location:'/howitworks', id:"asld86"},
+  {name : 'Sign In', location:'/auth/login', id:"kjsadf8"}
+]
 
-function HomeNavbar(props) {
+const UnAuthenticatedMobileNavOptions = [
+  {name : 'Explore', location:'/explore', id:"ias878"},
+  {name : 'How It Works', location:'/howitworks', id:"asld86"},
+  {name : 'Sign In', location:'/auth/login', id:"kjsadf8"}
+]
+
+const AuthenticatedMobileNavOptions = [
+  {name : 'Explore', location:'/explore', id:"ias878"},
+  {name : 'Dashboard', location:'/howitworks', id:"asld86"},
+  {name : 'Account', location:'/auth/login', id:"kjsadf8"}
+]
+
+function HomeNavbar() {
     const [navbaron, setNavbarOn] = useState(false)
 
-      const changeBackground = () => {
-        if (window.scrollY >= 100) {
-            setNavbarOn(true)
-        } else {
-            setNavbarOn(false)
-        }
+    const changeBackground = () => {
+      if (window.scrollY >= 100) {
+          setNavbarOn(true)
+      } else {
+          setNavbarOn(false)
+      }
     }
 
     window.addEventListener('scroll', changeBackground)
@@ -23,11 +41,21 @@ function HomeNavbar(props) {
             <div className="">
               <p>logo</p>
             </div>
-            <div className="lg:flex md:flex hidden">
-              <p>other</p>
+            <div className="lg:flex md:flex hidden items-center">
+              {
+                UnAuthenticatedNaOptions.map(option =>(
+                  <span key={option.id} className="mr-4 uppercase text-sm text-gray-700 font-semibold cursor-pointer">
+                    <p>{option.name}</p>
+                  </span>
+                ))
+              }
+              <span className="uppercase p-2 bg-yellow-400 rounded-sm text-gray-700 cursor-pointer hover:bg-yellow-500 text-sm font-semibold">
+                <p>JOIN COMMUNITY</p>
+              </span>
+      
             </div>
             <div className="lg:hidden md:hidden flex">
-              <MenuAlt1Icon height={24} width={24} className="text-gray-700" />
+              <NavbarDropdown options={UnAuthenticatedMobileNavOptions} title={<MenuAlt1Icon height={24} width={24} className="text-gray-700" />} />
             </div>
           </div>
         </nav>

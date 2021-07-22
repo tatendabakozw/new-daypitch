19b590b833d049d3028d60735d1a0c7f794ec6d3
tatendabakozw <tatendabakozw@gmail.com>
2021-07-23 01:15:14 +0200

    account page

---
+++ src/pages/account/Account.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeLayout from '../../layouts/HomeLayout/HomeLayout'
+
+function Account() {
+    return (
+        <HomeLayout>
+            account settings
+        </HomeLayout>
+    )
+}
+
+export default Account

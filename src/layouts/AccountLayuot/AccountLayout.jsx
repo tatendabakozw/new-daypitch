c5b22c84483d0336c5dd3e18a7aaa5dc2ac8d92f
tatendabakozw <tatendabakozw@gmail.com>
2021-08-01 12:48:00 +0200

    profile layout

--- src/layouts/AccountLayuot/AccountLayout.jsx
+++ src/layouts/AccountLayuot/AccountLayout.jsx
@@ -19,18 +19,18 @@ function AccountLayout({children}) {
     ]
 
     return (
-        <div className='flex flex-row pt-16'>
-            <div className="sidebar w-1/4 flex flex-col items-center pt-8 border-r border-gray-200">
+        <div className='flex md:flex-row flex-col pt-16'>
+            <div className="sidebar md:w-1/4 w-full flex md:flex-col flex-row items-center pt-8 md:border-r border-b border-gray-200 mb-4">
                 {
                     side_option.map(option=>(
-                        <span onClick={()=> history.push(option.location)} key={option.id} className={`${location.pathname === option.location ? "border-r-4 bg-gray-200 border-blue-900 " : "border-none "} p-4 w-full text-center hover:bg-gray-200 cursor-pointer`}>
+                        <span onClick={()=> history.push(option.location)} key={option.id} className={`${location.pathname === option.location ? "md:border-r-4 border-b-4 bg-gray-200 border-blue-900 " : "border-none "} p-4 w-full text-center hover:bg-gray-200 cursor-pointer`}>
                             {option.name}
                         </span>
                     ))
                 }
 
             </div>
-            <div className="body w-3/4">
+            <div className="body md:w-3/4 w-full">
                 {children}
             </div>
         </div>
--- src/pages/becomeAseller/BecomeASeller.jsx
+++ src/pages/becomeAseller/BecomeASeller.jsx
@@ -3,9 +3,10 @@ import HomeLayout from '../../layouts/HomeLayout/HomeLayout'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 import Tags from '../../components/tags/Tags'
-import { useStateValue } from '../../context/StateProvier'
 import axios from 'axios'
 import { apiUrl } from '../../helpers/apiUrl'
+import { useSelector } from 'react-redux'
+import AccountLayout from '../../layouts/AccountLayuot/AccountLayout'
 
 const categories = [
     { name: 'Programming and tech' },
@@ -26,9 +27,11 @@ function BecomeASeller() {
     const [school, setSchool] = useState('')
     const [pricerange, setPriceRange] = useState('')
     const [selected, setSelected] = useState(categories[0])
-    const [{token}] = useStateValue()
     const [loading, setLoading] = useState(false)
     const loggedInUser = localStorage.getItem('daypitch_user')
+
+    const userSignin = useSelector(state=> state.userCredsSignIn)
+    const {userInfo} = userSignin 
 
     const selectedTags = (tags) => {
         setCatTags(tags)
@@ -55,7 +58,7 @@ function BecomeASeller() {
             picture: JSON.parse(loggedInUser)?.propic
         },{
             headers: {
-                authorization: token
+                authorization: userInfo?.credential?.oauthIdToken
             }
         }).then(res=>{
             setLoading(false)
@@ -74,17 +77,17 @@ function BecomeASeller() {
 
     return (
         <HomeLayout>
-           <div className=" flex flex-col items-center px-4">
+            <AccountLayout>
+            <div className=" flex flex-col lg:px-32 md:px-16 px-4 items-center">
                <p className="py-16 text-xl font-semibold text-gray-700">Describe your service</p>
-
                {/* category */}
-               <div className="flex flex-col lg:w-2/5 md:w-2/3 w-full items-center">
+               <div className="flex flex-col  w-full items-center">
                     <div className="flex flex-col self-center bg-gray-50 w-full">
                             <p className="text-sm mb-2 text-gray-700 ml-4">Select service category</p>
                             <div className={`" w-full"`}>
                                 <Listbox value={selected} onChange={setSelected}>
                                     <div className="relative mt-1">
-                                    <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-sm border border-gray-300 cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+                                    <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left rounded-lg border border-gray-300 cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
                                         <span className="block truncate">{selected.name}</span>
                                         <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                                         <SelectorIcon
@@ -99,7 +102,7 @@ function BecomeASeller() {
                                         leaveFrom="opacity-100"
                                         leaveTo="opacity-0"
                                     >
-                                        <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-sm border border-gray-300 max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                        <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base rounded-lg border border-gray-300 max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                                         {categories.map((person, personIdx) => (
                                             <Listbox.Option
                                             key={personIdx}
@@ -141,7 +144,7 @@ function BecomeASeller() {
                </div>
 
                {/* tags */}
-               <div className="flex flex-col lg:w-2/5 md:w-2/3 w-full items-center">
+               <div className="flex flex-col w-full items-center">
                     <div className="flex flex-col self-center bg-gray-50 w-full">
                             {/* <p className="text-sm my-4 text-gray-700 ml-4 font-semibold">Search tags</p> */}
                             <Tags
@@ -152,7 +155,7 @@ function BecomeASeller() {
                </div>
 
                {/* description */}
-               <div className="flex flex-col lg:w-2/5 md:w-2/3 w-full items-center">
+               <div className="flex flex-col w-full items-center">
                     <div className="flex flex-col self-center bg-gray-50 w-full">
                         <p className="text-sm my-2 text-gray-700 ml-4">Description</p>
                         {/* <textarea 
@@ -164,7 +167,7 @@ function BecomeASeller() {
                         <textarea 
                             minLength={150}
                             cols="30" rows="7"
-                            className="p-2 border border-gray-300 outline-none rounded-sm"
+                            className="p-2 border border-gray-300 outline-none rounded-lg bg-gray-50"
                             placeholder="Describe yourself and/or your service with not less than 150 words"
                             onChange={e=> setDescription(e.target.value)}
                         />
@@ -172,20 +175,20 @@ function BecomeASeller() {
                </div>
 
                {/* education */}
-               <div className="flex flex-col lg:w-2/5 md:w-2/3 w-full items-center">
+               <div className="flex flex-col w-full items-center">
                     <div className="flex flex-col self-center bg-gray-50 w-full">
                         <p className="text-sm my-2 text-gray-700 ml-4">Education (Optional)</p>
                         <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
                             <input 
                                 type="text"
                                 placeholder="Level Reached"
-                                className="p-2 col-span-1 border border-gray-300 outline-none rounded-sm"
+                                className="p-2 col-span-1 border border-gray-300 bg-gray-50 outline-none rounded-lg"
                                 onChange={e=> setLevel(e.target.value)}
                             />
                             <input 
                                 type="text"
                                 placeholder="School Attended"
-                                className="p-2 border col-span-2 border-gray-300 outline-none rounded-sm"
+                                className="p-2 border col-span-2 border-gray-300 bg-gray-50 outline-none rounded-lg"
                                 onChange={e=> setSchool(e.target.value)}
                             />
                         
@@ -194,7 +197,7 @@ function BecomeASeller() {
                </div>
 
                 {/* price range */}
-               <div className="flex flex-col lg:w-2/5 md:w-2/3 w-full items-center mt-4">
+               <div className="flex flex-col w-full items-center mt-4">
                     <div className="flex flex-col self-center bg-gray-50 w-full">
                         <p className="text-sm my-2 text-gray-700 ml-4">Price range e.g $45/hour</p>
                         {/* <textarea 
@@ -206,7 +209,7 @@ function BecomeASeller() {
                         <input 
                             type="text"
                             placeholder="$35/hour"
-                            className="p-2 border col-span-2 border-gray-300 outline-none rounded-sm"
+                            className="p-2 border col-span-2 border-gray-300 bg-gray-50 outline-none rounded-lg"
                             onChange={e => setPriceRange(e.target.value)}
                         />
                     </div>
@@ -214,16 +217,17 @@ function BecomeASeller() {
                
 
                {/* //create profile button */}
-               <div className="flex flex-col lg:w-2/5 md:w-2/3 w-full items-center mt-8">
+               <div className="flex flex-col w-full items-center mt-8">
                     <div className="flex flex-col self-center bg-gray-50 w-full">
                         {
-                            loading ? (<p className="capitalize bg-blue-900 p-2 text-white rounded-sm text-center opacity-75 hover:bg-blue-800">Loading ...</p>) : (
-                                <span onClick={create_user_profile} className="capitalize bg-blue-900 p-2 text-white rounded-sm text-center cursor-pointer hover:bg-blue-800">Create My Profile</span>
+                            loading ? (<p className="capitalize bg-blue-900 p-2 text-white rounded-lg text-center opacity-75 hover:bg-blue-800">Loading ...</p>) : (
+                                <span onClick={create_user_profile} className="capitalize bg-blue-900 p-2 text-white rounded-lg text-center cursor-pointer hover:bg-blue-800">Create My Profile</span>
                             )
                         }
                     </div>
                </div>
            </div>
+            </AccountLayout>
         </HomeLayout>
     )
 }

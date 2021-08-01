f46e619741a91b5c3bf7c3402cd75a9c63fc9cbf
tatendabakozw <tatendabakozw@gmail.com>
2021-08-01 12:51:23 +0200

    borders account

--- src/layouts/AccountLayuot/AccountLayout.jsx
+++ src/layouts/AccountLayuot/AccountLayout.jsx
@@ -20,15 +20,14 @@ function AccountLayout({children}) {
 
     return (
         <div className='flex md:flex-row flex-col pt-16'>
-            <div className="sidebar md:w-1/4 w-full flex md:flex-col flex-row items-center pt-8 md:border-r border-b border-gray-200 mb-4">
+            <div className="sidebar md:w-1/4 w-full flex md:flex-col flex-row items-center pt-8 border-r border-gray-200 mb-4">
                 {
                     side_option.map(option=>(
-                        <span onClick={()=> history.push(option.location)} key={option.id} className={`${location.pathname === option.location ? "md:border-r-4 border-b-4 bg-gray-200 border-blue-900 " : "border-none "} p-4 w-full text-center hover:bg-gray-200 cursor-pointer`}>
+                        <span onClick={()=> history.push(option.location)} key={option.id} className={`${location.pathname === option.location ? "md:border-r-4 border-r-0 md:border-b-0 border-b-4 bg-gray-200 border-blue-900 " : "border-none "} p-4 w-full text-center hover:bg-gray-200 cursor-pointer`}>
                             {option.name}
                         </span>
                     ))
                 }
-
             </div>
             <div className="body md:w-3/4 w-full">
                 {children}

5c6c6e992e296103f6c0f05c0a0749b49f2799cd
tatendabakozw <tatendabakozw@gmail.com>
2021-08-01 21:34:24 +0200

    api url in services

--- src/helpers/apiUrl.js
+++ src/helpers/apiUrl.js
@@ -1,2 +1,2 @@
-export const apiUrl = `http://localhost:5500/api/v1`
-// export const apiUrl = "https://new-daypitch.herokuapp.com/api/v1"
+// export const apiUrl = `http://localhost:5500/api/v1`
+export const apiUrl = "https://new-daypitch.herokuapp.com/api/v1"

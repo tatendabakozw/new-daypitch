1c221742ff73f2845826963f1cb6a71d0401b173
tatendabakozw <tatendabakozw@gmail.com>
2021-08-01 21:10:37 +0200

    loading of services fixed

--- src/redux/reducers/serviceReducer.js
+++ src/redux/reducers/serviceReducer.js
@@ -1,4 +1,4 @@
-import { ALL_SERVICE_GET_FAIL, ALL_SERVICE_GET_SUCSESS, SERVICE_GET_FAIL, SERVICE_GET_REQUEST, SERVICE_GET_SUCSESS } from "../constants/serviceConstants";
+import { ALL_SERVICE_GET_FAIL, ALL_SERVICE_GET_REQUEST, ALL_SERVICE_GET_SUCSESS, SERVICE_GET_FAIL, SERVICE_GET_REQUEST, SERVICE_GET_SUCSESS } from "../constants/serviceConstants";
 
 //get service
 export const singleServiceReducer = (state={}, action) =>{
@@ -17,11 +17,11 @@ export const singleServiceReducer = (state={}, action) =>{
 //get all services
 export const getAllServicesReducer = (state={}, action) =>{
     switch(action.type){
-        case ALL_SERVICE_GET_FAIL:
+        case ALL_SERVICE_GET_REQUEST:
             return {loading: true}
         case ALL_SERVICE_GET_SUCSESS:
             return {loading: false, all_services: action.payload}
-        case SERVICE_GET_FAIL:
+        case ALL_SERVICE_GET_FAIL:
             return {loading: false, error: action.payload}
         default:
             return state

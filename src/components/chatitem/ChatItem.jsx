cbd64c704b70f7e67d8dd940a2cc2bc158e6c33a
tatendabakozw <tatendabakozw@gmail.com>
2021-08-05 13:43:12 +0200

    chat page users name arrange

--- src/components/chatitem/ChatItem.jsx
+++ src/components/chatitem/ChatItem.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import image from '../../images/IMG_5117_1_1.jpg'
 
-function ChatItem() {
+function ChatItem({sender_pic, message,sender_name, sender_id}) {
     return (
         <div className="grid grid-cols-5 border-b border-gray-200 p-2 cursor-pointer hover:bg-gray-50">
             <div className="h-16 w-16 col-span-1 rounded-full bg-gray-200 mr-2 overflow-hidden">
-                <img src={image} alt="user_image" className="object-contain" />
+                <img src={sender_pic} alt="user_image" className="object-contain" />
             </div>
             <div className="col-span-4 flex flex-col justify-center px-2 overflow-ellipsis">
-                <p className="text-gray-700 font-semibold text-lg">Full name</p>
-                <p className="truncate text-sm text-gray-600">iam a new mekahkfjh asjkflhkjasdhfkjl hagfhjkasfjkahskjldfhlkjasfh  asflhjkashflkjhasdfjhlkjashd</p>
+                <p className="text-gray-700 font-semibold text-lg">{sender_name}</p>
+                <p className="truncate text-sm text-gray-600">{message}</p>
             </div>
         </div>
     )
--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -2,11 +2,55 @@ import React from 'react'
 import { useState } from 'react'
 import ChatItem from '../../components/chatitem/ChatItem'
 import ChatLayout from '../../layouts/chatlayout/ChatLayout'
-import {ChevronLeftIcon} from '@heroicons/react/outline'
+import {ChevronLeftIcon, ChatIcon} from '@heroicons/react/outline'
+import pic from '../../images/IMG_5117_1_1.jpg'
+import image from '../../images/man.png'
+import { useHistory } from 'react-router-dom'
+
+const messages = [
+    {
+        message: 'hi, whats app, can i borrow some money',
+        sent_to: 'me',
+        sender_name: 'tatenda bako',
+        receiver_name: 'tafara bako',
+        sender_pic: pic,
+        id: 1,
+        date: 1
+    },
+    {
+        message: 'this is a test message to see if everything works',
+        sent_to: 'me',
+        sender_name: 'tafara bako',
+        receiver_name: 'tatenda bako',
+        sender_pic: image,
+        id: 2,
+        date: 2,
+    },
+    {
+        message: 'hi, whats app, can i borrow some money',
+        sent_to: 'me',
+        sender_name: 'tafara bako',
+        receiver_name: 'tatenda bako',
+        sender_pic: pic,
+        id: 4,
+        date: 3
+    },
+    {
+        message: 'this is a test message to see if everything works',
+        sent_to: 'me',
+        sender_name: 'tatenda bako',
+        receiver_name: 'tafara bako',
+        sender_pic: image,
+        id: 4,
+        date: 4,
+    }
+]
 
 function Chat() {
     
     const [mobile_on, stMobileOn] = useState(false)
+    const history = useHistory()
+    const [current_chat_id, setCurrentChat_id] = useState()
 
     return (
         <ChatLayout>
@@ -17,9 +61,20 @@ function Chat() {
                             <div className="flex flex-col rounded-lg p-2">
                                 <input type="text" placeholder="search" className="p-2 rounded-lg border border-gray-200 outline-none w-full" />
                             </div>
-                            <span onClick={()=> stMobileOn(true)}>
-                                <ChatItem/>
-                            </span>
+                            {
+                                messages.map(message=>(
+                                    <span onClick={()=> {
+                                        stMobileOn(true)
+                                        setCurrentChat_id(message.id)
+                                    }}>
+                                        <ChatItem
+                                            sender_id={message.id}
+                                            sender_name={message.sender_name}
+                                            message={message.message}
+                                        />
+                                    </span>
+                                ))
+                            }
                         </div>
                     </div>
                     <div className={`${mobile_on ? "block" : "hidden" } md:w-4/5 w-full  min-h-screen bg-gray-50 p-4`}>
@@ -37,13 +92,44 @@ function Chat() {
                             <div className="flex flex-col rounded-lg p-2">
                                 <input type="text" placeholder="search" className="p-2 rounded-lg border border-gray-200 outline-none w-full" />
                             </div>
-                            <span onClick={()=> stMobileOn(true)}>
-                                <ChatItem/>
-                            </span>
+                            {
+                                messages.map(message=>(
+                                    <span onClick={()=> {
+                                        stMobileOn(true)
+                                        setCurrentChat_id(message.id)
+                                    }}>
+                                        <ChatItem
+                                            sender_id={message.id}
+                                            sender_name={message.sender_name}
+                                            message={message.message}
+                                        />
+                                    </span>
+                                ))
+                            }
                         </div>
                     </div>
-                    <div className={`flex flex-col w-4/5  min-h-screen bg-gray-white p-4 bg-green-600`}>
-                        alksdjfkl;asjflksj
+                    <div className={`flex flex-col w-4/5  min-h-screen bg-gray-white p-4 bg-whitewhite`}>
+                        {
+                            current_chat_id ? (<>
+                                {
+                                    messages.map((message, index)=>(
+                                        <div className="flex flex-col w-full justify-between" key={index}>
+                                            {message.receiver_name === "tatenda bako" ? (<div className="flex flex-row justify-end w-full">
+                                                <p className="text-right my-4 bg-blue-900 text-white max-w-4xl rounded-lg p-4">{message.message}</p>
+                                            </div>) : null}
+                                            {message.receiver_name === "tafara bako" ? (<div className="flex flex-row justify-start">
+                                                <p className="text-left my-4 bg-gray-100 text-gray-700 max-w-4xl rounded-lg p-4">{message.message}</p>
+                                            </div>) : null}
+                                        </div>
+                                    ))
+                                }
+                            </>) : (<div className="grid items-center content-center my-auto">
+                                <div className="flex flex-col items-center my-auto">
+                                    <ChatIcon height={96} width={96} className="text-blue-900" />
+                                    <p>Click a chat to view</p>
+                                </div>
+                            </div>)
+                        }
                     </div>
                 </div>
             </div>

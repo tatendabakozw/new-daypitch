c035b9c260ee678c023260c758238d973e052347
tatendabakozw <tatendabakozw@gmail.com>
2021-08-06 19:00:43 +0200

    chat functionality adde

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 import { ChatEngine, getOrCreateChat } from 'react-chat-engine';
-import ChatFeed from '../../components/ChatFeed/ChatFeed';
 import HomeNavbar from '../../components/navigation/HomeNavbar';
+import './ChatStyles.css'
 
 function Chat() {
 
     const [username, setUsername] = useState('')
 
-    function createDirectChat(creds) {
+	function createDirectChat(creds) {
 		getOrCreateChat(
 			creds,
 			{ is_direct_chat: true, usernames: [username] },
@@ -15,18 +15,19 @@ function Chat() {
 		)
 	}
 
-    function renderChatForm(creds) {
+	function renderChatForm(creds) {
 		return (
-			<div>
+			<form onSubmit={ () => createDirectChat(creds)} className="flex flex-row items-center border border-gray-200 rounded-full px-2 mb-8">
 				<input 
-					placeholder='Username' 
+					placeholder='type username' 
 					value={username} 
 					onChange={(e) => setUsername(e.target.value)} 
+					className="p-2 w-full rounded-full outline-none"
 				/>
-				<button onClick={() => createDirectChat(creds)}>
-					Create
+				<button type="submit" className="text-gray-500">
+					search
 				</button>
-			</div>
+			</form>
 		)
 	}
 
@@ -40,7 +41,8 @@ function Chat() {
                 userName='bako'
                 userSecret='123456'
                 projectID='908aeb92-95e4-4503-9f57-de1b322f4dee'
-                renderChatFeed = {(feedProps)=> <ChatFeed {...feedProps} />}
+                // renderChatFeed = {(feedProps)=> <ChatFeed {...feedProps} />}
+				renderNewChatForm={(creds) => renderChatForm(creds)}
             />
             </div>
         </div>

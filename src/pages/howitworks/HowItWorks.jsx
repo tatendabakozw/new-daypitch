6608939c94de50be4a3912f1d35519058236c783
tatendabakozw <tatendabakozw@gmail.com>
2021-08-07 06:32:56 +0200

    how it works page redesign

--- src/pages/howitworks/HowItWorks.jsx
+++ src/pages/howitworks/HowItWorks.jsx
@@ -9,28 +9,111 @@ import HomeLayout from '../../layouts/HomeLayout/HomeLayout'
 function HowItWorks() {
     return (
         <HomeLayout>
-            <div className="flex flex-col md:px-48 px-8 bg-gray-50 dark:bg-gray-900 pb-16 pt-16">
-                {/* <p className="bg-blue-100 self-center text-sm font-semibold text-blue-900 p-2 rounded-sm">How it works?</p> */}
-                <div className="min-h-screen flex flex-row items-center w-full">
-                    <div className="flex flex-col w-full justify-between">
-                    {/* <p className="text-gray-900 md:text-2xl dark:text-gray-200 text-sm text-center font-semibold self-center mt-4 mb-16 capitalize">How it works</p> */}
-                    <div className="flex md:flex-row flex-col items-center justify-between">
-                        <HowItem
-                            pic={search}
-                            text="Search professional from around your area"
-                        />
-                        <HowItem
-                            pic={referal}
-                            text="Professionals create accounts and manage their services, while users can use referrals to connect professionals and clients"
-                        />
-                        <HowItem
-                            pic={analytics}
-                            text="Analyse growth of your business and manage your customers using analytics dashboard"
-                        />
+            <div className="flex flex-col md:px-24 px-8 bg-gray-50 dark:bg-gray-900 pb-16 pt-16">
+
+
+            <div className="relative py-16 overflow-hidden">
+                <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+                    <div className="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
+                    <svg
+                        className="absolute top-12 left-full transform translate-x-32"
+                        width={404}
+                        height={384}
+                        fill="none"
+                        viewBox="0 0 404 384"
+                    >
+                        <defs>
+                        <pattern
+                            id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                            x={0}
+                            y={0}
+                            width={20}
+                            height={20}
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <rect x={0} y={0} width={4} height={4} className="text-yellow-200" fill="currentColor" />
+                        </pattern>
+                        </defs>
+                        <rect width={404} height={384} fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+                    </svg>
+                    <svg
+                        className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
+                        width={404}
+                        height={384}
+                        fill="none"
+                        viewBox="0 0 404 384"
+                    >
+                        <defs>
+                        <pattern
+                            id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                            x={0}
+                            y={0}
+                            width={20}
+                            height={20}
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <rect x={0} y={0} width={4} height={4} className="text-blue-200" fill="currentColor" />
+                        </pattern>
+                        </defs>
+                        <rect width={404} height={384} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+                    </svg>
+                    <svg
+                        className="absolute bottom-12 left-full transform translate-x-32"
+                        width={404}
+                        height={384}
+                        fill="none"
+                        viewBox="0 0 404 384"
+                    >
+                        <defs>
+                        <pattern
+                            id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                            x={0}
+                            y={0}
+                            width={20}
+                            height={20}
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                        </pattern>
+                        </defs>
+                        <rect width={404} height={384} fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+                    </svg>
                     </div>
                 </div>
-                </div>
-                <p className="bg-blue-100 self-center text-sm my-16 font-semibold text-blue-900  p-2 rounded-sm">Daypitch overflow</p>
+                <div className="relative px-4 sm:px-6 lg:px-8">
+                    <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+                        <div className="text-center">
+                        <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">Daypitch</h2>
+                        <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+                            Be your own boss.
+                        </p>
+                        <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+                            Work from anywhere, at your own time. And be your own boss
+                        </p>
+                        </div>
+                    </div>
+                        <div className="min-h-screen flex flex-row items-center w-full">
+                            <div className="flex flex-col w-full justify-between">
+                                {/* <p className="text-gray-900 md:text-2xl dark:text-gray-200 text-sm text-center font-semibold self-center mt-4 mb-16 capitalize">How it works</p> */}
+                                <div className="flex md:flex-row flex-col items-center justify-between">
+                                    <HowItem
+                                        pic={search}
+                                        text="Search professional from around your area"
+                                    />
+                                    <HowItem
+                                        pic={referal}
+                                        text="Professionals create accounts and manage their services, while users can use referrals to connect professionals and clients"
+                                    />
+                                    <HowItem
+                                        pic={analytics}
+                                        text="Analyse growth of your business and manage your customers using analytics dashboard"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col">
+                    <p className="bg-blue-100 self-center text-sm my-16 font-semibold text-blue-900  p-2 rounded-sm">Daypitch overflow</p>
                 <div className="below min-h-screen">
                     <div className="1 grid md:grid-cols-2 grid-cols-1 md:mb-32 mb-16 items-center gap-24">
                         <div className="left col-span-1">
@@ -53,6 +136,8 @@ function HowItWorks() {
                         </div>
                     </div>
                 </div>
+                    </div>
+                </div>        
             </div>
         </HomeLayout>
     )

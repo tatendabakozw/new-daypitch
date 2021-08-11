6c8770cccac674d2633010e5eddb129c7bda0b7b
tatendabakozw <tatendabakozw@gmail.com>
2021-08-11 04:35:20 +0200

    layout colour fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -63,7 +63,7 @@ const navigation = {
 
 export default function Footer() {
   return (
-    <footer className="bg-white">
+    <footer className="bg-gray-50">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
           {navigation.main.map((item) => (
@@ -76,13 +76,13 @@ export default function Footer() {
         </nav>
         <div className="mt-8 flex justify-center space-x-6">
           {navigation.social.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+            <a key={item.name} href={item.href} className="text-gray-500 hover:text-gray-700">
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
             </a>
           ))}
         </div>
-        <p className="mt-8 text-center text-base text-gray-400">&copy; {new Date().getFullYear()}{" "}Daypitch. A product of Perkskick Group{" "}</p>
+        <p className="mt-8 text-center text-sm text-gray-500">&copy; {new Date().getFullYear()}{" "}Daypitch. A product of Perkskick Group{" "}</p>
       </div>
     </footer>
   )

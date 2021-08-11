5a3264d9b59751311f8a89a1da3512c707bbbf2c
tatendabakozw <tatendabakozw@gmail.com>
2021-08-11 05:37:17 +0200

    notifications dropdown on navbar

---
+++ src/components/navigation/NavbarNotification.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import {ChatAlt2Icon, BellIcon} from '@heroicons/react/outline'
+import { Fragment, useState } from 'react'
+import { Transition } from '@headlessui/react'
+import { CheckCircleIcon, InboxIcon } from '@heroicons/react/outline'
+import { XIcon } from '@heroicons/react/solid'
+
+function NavbarNotification() {
+    const [show, setShow] = useState(false)
+
+  return (
+    <>
+      {/* Global notification live region, render this permanently at the end of the document */}
+      <span onClick={()=>setShow(true)}><BellIcon height={20} width={20} className="text-gray-700" /></span>
+      <div
+        aria-live="assertive"
+        className="fixed inset-12 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start"
+      >
+        <div className="w-full flex flex-col items-center space-y-4 sm:items-end">
+          {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
+          <Transition
+            show={show}
+            as={Fragment}
+            enter="transform ease-out duration-300 transition"
+            enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+            enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5">
+                <div className="bg-gray-100 p-4 flex flex-row items-center justify-between">
+                    <p>Notifications</p>
+                    <div className="ml-4 flex-shrink-0 flex">
+                    <button
+                      className="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      onClick={() => {
+                        setShow(false)
+                      }}
+                    >
+                      <span className="sr-only">Close</span>
+                      <XIcon className="h-5 w-5" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+              <div className="p-4">
+                <div className="flex items-start">
+                  <div className="flex-shrink-0 pt-0.5">
+                    <img
+                      className="h-10 w-10 rounded-full"
+                      src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                      alt=""
+                    />
+                  </div>
+                  <div className="ml-3 w-0 flex-1">
+                    <p className="text-sm font-medium text-gray-900">Emilia Gates</p>
+                    <p className="mt-1 text-sm text-gray-500">Sent you a new contract</p>
+                    <div className="mt-4 flex">
+                      <button
+                        type="button"
+                        className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-blue-900 hover:bg-blue-800 focus:outline-none"
+                      >
+                        View
+                      </button>
+                      <button
+                        type="button"
+                        className="ml-3 inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none"
+                      >
+                        Decline
+                      </button>
+                    </div>
+                  </div>
+                 
+                </div>
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default NavbarNotification

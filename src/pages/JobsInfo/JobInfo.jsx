6e375ebc3dfd7ae2b3e74d52d7b5835888d117f6
tatendabakozw <tatendabakozw@gmail.com>
2021-08-30 07:26:45 +0200

    fix: job descriprion page done

--- src/pages/JobsInfo/JobInfo.jsx
+++ src/pages/JobsInfo/JobInfo.jsx
@@ -138,29 +138,6 @@ export default function JobInfo() {
                                                     {job.details}
                                                 </dd>
                                             </div>
-                                            <div className="sm:col-span-2">
-                                                <dt className="text-sm font-medium text-gray-500">Attachments</dt>
-                                                <dd className="mt-1 text-sm text-gray-900">
-                                                    <ul role="list" className="border border-gray-200 rounded-md divide-y divide-gray-200">
-                                                        {attachments.map((attachment) => (
-                                                            <li
-                                                                key={attachment.name}
-                                                                className="pl-3 pr-4 py-3 flex items-center justify-between text-sm"
-                                                            >
-                                                                <div className="w-0 flex-1 flex items-center">
-                                                                    <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                                                    <span className="ml-2 flex-1 w-0 truncate">{attachment.name}</span>
-                                                                </div>
-                                                                <div className="ml-4 flex-shrink-0">
-                                                                    <a href={attachment.href} className="font-medium text-blue-900 hover:text-blue-500">
-                                                                        Download
-                                                                    </a>
-                                                                </div>
-                                                            </li>
-                                                        ))}
-                                                    </ul>
-                                                </dd>
-                                            </div>
                                         </dl>
                                     </div>
                                 </div>
--- src/pages/my_jobs/MyJobs.jsx
+++ src/pages/my_jobs/MyJobs.jsx
@@ -72,22 +72,6 @@ function MyJobs() {
                     </>
                 )}
             </Disclosure>
-            <Disclosure as="div" className="mt-2">
-                {({ open }) => (
-                    <>
-                        <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-blue-900 bg-blue-100 rounded-sm hover:bg-blue-200 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75">
-                            <span>Website develpment</span>
-                            <ChevronUpIcon
-                                className={`${open ? 'transform rotate-180' : ''
-                                    } w-5 h-5 text-blue-500`}
-                            />
-                        </Disclosure.Button>
-                        <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                            No.
-                        </Disclosure.Panel>
-                    </>
-                )}
-            </Disclosure>
         </ContractsLayout>
     )
 }

c3f0b12a792e62f6dbd156557bc465a699dc2d5f
tatendabakozw <tatendabakozw@gmail.com>
2021-09-24 22:10:51 +0200

    new job details

--- src/pages/CreateJob/Listings.jsx
+++ src/pages/CreateJob/Listings.jsx
@@ -85,7 +85,7 @@ export default function Listings() {
                                                                     <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{job?.job?.name}</dd>
                                                                 </div>
                                                                 <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                                                    <dt className="text-sm font-medium text-gray-500">Contract name</dt>
+                                                                    <dt className="text-sm font-medium text-gray-500">Job name</dt>
                                                                     <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{job?.job?.title}</dd>
                                                                 </div>
                                                                 <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">

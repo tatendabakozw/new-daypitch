ff021f3929c2450c212378b355268e73b5c69677
tatendabakozw <tatendabakozw@gmail.com>
2021-09-25 02:53:44 +0200

    fix: contact form dummy send

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,6 +2,8 @@ import { MailIcon, PhoneIcon } from "@heroicons/react/outline";
 import HomeLayout from "../../layouts/HomeLayout/HomeLayout";
 import { Input } from "@chakra-ui/react";
 import { useState } from "react";
+import Success from "../../components/alert/Success";
+import Error from "../../components/alert/Error";
 
 export default function Contact() {
   const [firstname, setFirstname] = useState("");
@@ -10,6 +12,17 @@ export default function Contact() {
   const [code, setSubject] = useState("");
   const [number, setNumber] = useState("");
   const [details, setDetails] = useState("");
+  const [message, setMessage] = useState("");
+  const [err, setErr] = useState("");
+
+  const send_feedback_message = (e) => {
+    e.preventDefault();
+    if (!details) {
+      setErr("Enter all details!");
+    } else {
+      setMessage("Thank you for contacting us");
+    }
+  };
 
   return (
     <HomeLayout>
@@ -280,11 +293,7 @@ export default function Contact() {
                     <h3 className="text-lg font-medium text-gray-900">
                       Send us a message
                     </h3>
-                    <form
-                      action="/"
-                      method="POST"
-                      className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
-                    >
+                    <form className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
                       <div>
                         <label
                           htmlFor="first-name"
@@ -403,16 +412,20 @@ export default function Contact() {
                             id="message"
                             name="message"
                             rows={4}
-                            onChange={e => setDetails(e.target.value)}
+                            onChange={(e) => setDetails(e.target.value)}
                             className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border border-warm-gray-300 rounded-md"
                             aria-describedby="message-max"
                             defaultValue={""}
                           />
                         </div>
                       </div>
+                      <div className="w-full flex flex-col">
+                        {message && <Success text={message} />}
+                        {err && <Error text={err} />}
+                      </div>
                       <div className="sm:col-span-2 sm:flex sm:justify-end">
                         <button
-                          type="submit"
+                          onClick={send_feedback_message}
                           className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-900 hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 sm:w-auto"
                         >
                           Submit

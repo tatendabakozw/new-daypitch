c62739554f085673a0f75aa170300f2ac549b315
tatendabakozw <tatendabakozw@gmail.com>
2023-01-11 08:48:42 +0200

    feat: added new url

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -94,6 +94,12 @@ export default function Footer() {
           ))}
         </div>
         <p className="mt-8 text-center text-sm text-gray-500">&copy; {new Date().getFullYear()}{" "}Daypitch. A product of Perkskick Group{" "}</p>
+        <a
+          href="https://www.ladworx.com"
+          className="text-gray-600 hover:text-gray-900"
+        >
+          By LadWorx
+        </a>
       </div>
     </footer>
   )
